aa1f5a599cb9323e3aad4d6767f4f9b4de0b9f87
JulesDrouet <jules.drouet44@gmail.com>
2023-09-29 10:15:32 +0200

    exercices 1 et 2

---
+++ exercice 2/exercice 2/Program.cs
@@ -0,0 +1,18 @@
+﻿namespace exercice_2
+{
+    internal class Program
+    {
+        public static void Main()
+        {
+            Console.WriteLine("écrivez un mot:");
+            var userInput = Console.ReadLine();
+            var userInputUpper = ConvertToUpper(userInput);
+            Console.WriteLine("Mot en majuscule: "+userInputUpper);
+        }
+
+        public static string ConvertToUpper(string text)
+        {
+            return text.ToUpper();
+        }
+    }
+}
