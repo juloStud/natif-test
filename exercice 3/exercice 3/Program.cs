d2d878a914dee7fb95faeb6f1b99a049c00390dd
JulesDrouet <jules.drouet44@gmail.com>
2023-09-29 11:32:27 +0200

    exercice 3

---
+++ exercice 3/exercice 3/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+
+public class Program
+{
+    public static void Main()
+    {
+        Console.Write("Entrez un nombre: ");
+        if (int.TryParse(Console.ReadLine(), out int number))
+        {
+            string output = (number % 2 == 0) ? "pair" : "impair";
+            Console.WriteLine(output);
+        }
+        else
+        {
+            Console.WriteLine("Veuillez entrer un nombre entier valide.");
+        }
+    }
+}
